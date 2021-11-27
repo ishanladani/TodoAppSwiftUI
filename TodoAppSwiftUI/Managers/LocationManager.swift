51d6849ba6d15a2f38d301e1103d1224de848d50
Ishan Ladani <ishanladani16@gmail.com>
2021-11-27 12:44:32 +0530

    Logic For Value Store

---
+++ TodoAppSwiftUI/Managers/LocationManager.swift
@@ -0,0 +1,39 @@
+//
+//  LocationManager.swift
+//  TodoAppSwiftUI
+//
+//  Created by ISHAN LADANI on 27/11/21.
+//
+
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+  var locationManager = CLLocationManager()
+  @Published var authorized = false
+
+  override init() {
+    super.init()
+    locationManager.delegate = self
+    if locationManager.authorizationStatus == .authorizedWhenInUse {
+      authorized = true
+      locationManager.startMonitoringSignificantLocationChanges()
+    }
+  }
+
+  func requestAuthorization() {
+    locationManager.requestWhenInUseAuthorization()
+  }
+}
+
+// MARK: - CLLocationManagerDelegate
+extension LocationManager: CLLocationManagerDelegate {
+  func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+    if locationManager.authorizationStatus == .authorizedWhenInUse ||
+      locationManager.authorizationStatus == .authorizedAlways {
+      authorized = true
+    } else {
+      authorized = false
+    }
+  }
+}
+
---
+++ TodoAppSwiftUI/Managers/NotificationManager.swift
@@ -0,0 +1,111 @@
+//
+//  NotificationManager.swift
+//  TodoAppSwiftUI
+//
+//  Created by ISHAN LADANI on 27/11/21.
+//
+
+import Foundation
+import UserNotifications
+import CoreLocation
+
+enum NotificationManagerConstants {
+  static let timeBasedNotificationThreadId =
+    "TimeBasedNotificationThreadId"
+  static let calendarBasedNotificationThreadId =
+    "CalendarBasedNotificationThreadId"
+  static let locationBasedNotificationThreadId =
+    "LocationBasedNotificationThreadId"
+}
+
+class NotificationManager: ObservableObject {
+  static let shared = NotificationManager()
+  @Published var settings: UNNotificationSettings?
+
+  func requestAuthorization(completion: @escaping  (Bool) -> Void) {
+    UNUserNotificationCenter.current()
+      .requestAuthorization(options: [.alert, .sound, .badge]) { granted, _  in
+        self.fetchNotificationSettings()
+        completion(granted)
+      }
+  }
+
+  func fetchNotificationSettings() {
+    // 1
+    UNUserNotificationCenter.current().getNotificationSettings { settings in
+      // 2
+      DispatchQueue.main.async {
+        self.settings = settings
+      }
+    }
+  }
+
+  func removeScheduledNotification(task: Task) {
+    UNUserNotificationCenter.current()
+      .removePendingNotificationRequests(withIdentifiers: [task.id])
+  }
+
+  // 1
+  func scheduleNotification(task: Task) {
+    // 2
+    let content = UNMutableNotificationContent()
+    content.title = task.name
+    content.body = "Gentle reminder for your task!"
+    content.categoryIdentifier = "OrganizerPlusCategory"
+    let taskData = try? JSONEncoder().encode(task)
+    if let taskData = taskData {
+      content.userInfo = ["Task": taskData]
+    }
+
+    // 3
+    var trigger: UNNotificationTrigger?
+    switch task.reminder.reminderType {
+    case .time:
+      if let timeInterval = task.reminder.timeInterval {
+        trigger = UNTimeIntervalNotificationTrigger(
+          timeInterval: timeInterval,
+          repeats: task.reminder.repeats)
+      }
+      content.threadIdentifier =
+        NotificationManagerConstants.timeBasedNotificationThreadId
+    case .calendar:
+      if let date = task.reminder.date {
+        trigger = UNCalendarNotificationTrigger(
+          dateMatching: Calendar.current.dateComponents(
+            [.day, .month, .year, .hour, .minute],
+            from: date),
+          repeats: task.reminder.repeats)
+      }
+      content.threadIdentifier =
+        NotificationManagerConstants.calendarBasedNotificationThreadId
+    case .location:
+      // 1
+      guard CLLocationManager().authorizationStatus == .authorizedWhenInUse else {
+        return
+      }
+      // 2
+      if let location = task.reminder.location {
+        // 3
+        let center = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+        let region = CLCircularRegion(center: center, radius: location.radius, identifier: task.id)
+        trigger = UNLocationNotificationTrigger(region: region, repeats: task.reminder.repeats)
+      }
+      content.threadIdentifier =
+      NotificationManagerConstants.locationBasedNotificationThreadId
+    }
+
+    // 4
+    if let trigger = trigger {
+      let request = UNNotificationRequest(
+        identifier: task.id,
+        content: content,
+        trigger: trigger)
+      // 5
+      UNUserNotificationCenter.current().add(request) { error in
+        if let error = error {
+          print(error)
+        }
+      }
+    }
+  }
+}
