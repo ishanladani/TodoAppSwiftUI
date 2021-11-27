22bbee860a9d6de56e66b14961ab88ed0fd9605c
Ishan Ladani <ishanladani16@gmail.com>
2021-11-27 13:26:33 +0530

    Create Task

---
+++ TodoAppSwiftUI/Managers/SliderValue.swift
@@ -0,0 +1,14 @@
+//
+//  SliderValue.swift
+//  TodoAppSwiftUI
+//
+//  Created by ISHAN LADANI on 27/11/21.
+//
+
+import Foundation
+import SwiftUI
+
+// make your observable double for the slider value:
+class SliderValue: ObservableObject {
+    @Published var position: Double = 11.0
+}
---
+++ TodoAppSwiftUI/Views/CreateTaskView.swift
@@ -0,0 +1,163 @@
+//
+//  CreateTaskView.swift
+//  TodoAppSwiftUI
+//
+//  Created by ISHAN LADANI on 27/11/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct CreateTaskView: View {
+  @State var taskName: String = ""
+  @State var reminderEnabled = false
+  @State var selectedTrigger = ReminderType.time
+  @State var timeDurationIndex: Int = 0
+  @State private var dateTrigger = Date()
+  @State private var shouldRepeat = false
+  @State private var latitude: String = ""
+  @State private var longitude: String = ""
+  @State private var radius: String = ""
+  @Environment(\.presentationMode) var presentationMode
+
+  let triggers = ["Time", "Calendar", "Location"]
+  let timeDurations: [Int] = Array(1...59)
+  var body: some View {
+    NavigationView {
+      Form {
+        Section {
+          HStack {
+            Spacer()
+            Text("Add Task")
+              .font(.title)
+              .padding()
+            Spacer()
+            Button("Save") {
+              TaskManager.shared.addNewTask(taskName, makeReminder())
+              presentationMode.wrappedValue.dismiss()
+            }
+            .disabled(taskName.isEmpty ? true : false)
+            .padding()
+          }
+          VStack {
+            TextField("Enter name for the task", text: $taskName)
+              .padding(.vertical)
+            Toggle(isOn: $reminderEnabled) {
+              Text("Add Reminder")
+            }
+            .padding(.vertical)
+
+            if reminderEnabled {
+              ReminderView(
+                selectedTrigger: $selectedTrigger,
+                timeDurationIndex: $timeDurationIndex,
+                triggerDate: $dateTrigger,
+                shouldRepeat: $shouldRepeat,
+                latitude: $latitude,
+                longitude: $longitude,
+                radius: $radius)
+                .navigationBarHidden(true)
+                .navigationTitle("")
+            }
+            Spacer()
+          }
+          .padding()
+        }
+      }
+      .navigationBarTitle("")
+      .navigationBarHidden(true)
+    }
+  }
+
+  func makeReminder() -> Reminder? {
+    guard reminderEnabled else {
+      return nil
+    }
+    var reminder = Reminder()
+    reminder.reminderType = selectedTrigger
+    switch selectedTrigger {
+    case .time:
+      reminder.timeInterval = TimeInterval(timeDurations[timeDurationIndex] * 60)
+    case .calendar:
+      reminder.date = dateTrigger
+    case .location:
+      if let latitude = Double(latitude),
+        let longitude = Double(longitude),
+        let radius = Double(radius) {
+        reminder.location = LocationReminder(
+          latitude: latitude,
+          longitude: longitude,
+          radius: radius)
+      }
+    }
+    reminder.repeats = shouldRepeat
+    return reminder
+  }
+}
+
+struct CreateTaskView_Previews: PreviewProvider {
+  static var previews: some View {
+    CreateTaskView()
+  }
+}
+
+struct ReminderView: View {
+  @Binding var selectedTrigger: ReminderType
+  @Binding var timeDurationIndex: Int
+  @Binding var triggerDate: Date
+  @Binding var shouldRepeat: Bool
+  @Binding var latitude: String
+  @Binding var longitude: String
+  @Binding var radius: String
+  @StateObject var locationManager = LocationManager()
+
+  var body: some View {
+    VStack {
+      Picker("Notification Trigger", selection: $selectedTrigger) {
+        Text("Time").tag(ReminderType.time)
+        Text("Date").tag(ReminderType.calendar)
+        Text("Location").tag(ReminderType.location)
+      }
+      .pickerStyle(SegmentedPickerStyle())
+      .padding(.vertical)
+      if selectedTrigger == ReminderType.time {
+        Picker("Time Interval", selection: $timeDurationIndex) {
+          ForEach(1 ..< 59) { i in
+            if i == 1 {
+              Text("\(i) minute").tag(i)
+            } else {
+              Text("\(i) minutes").tag(i)
+            }
+          }
+          .navigationBarHidden(true)
+          .padding(.vertical)
+        }
+      } else if selectedTrigger == ReminderType.calendar {
+        DatePicker("Please enter a date", selection: $triggerDate)
+          .labelsHidden()
+          .padding(.vertical)
+      } else {
+        VStack {
+          if !locationManager.authorized {
+            Button(
+              action: {
+                locationManager.requestAuthorization()
+              },
+              label: {
+                Text("Request Location Authorization")
+              })
+          } else {
+            TextField("Enter Latitude", text: $latitude)
+            TextField("Enter Longitude", text: $longitude)
+            TextField("Enter Radius", text: $radius)
+          }
+        }
+        .padding(.vertical)
+      }
+      Toggle(isOn: $shouldRepeat) {
+        Text("Repeat Notification")
+      }
+    }
+  }
+}
+
